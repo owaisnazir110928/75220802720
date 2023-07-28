d5d32242d5a26098d9241c3c31ca2606ae39a4d1
Owais Nazir Dar <63143570+owaisnazir110928@users.noreply.github.com>
2023-07-28 16:58:52 +0530

    ..

--- TRAIN_SYSTEM/src/Components/TrainDetails.jsx
+++ TRAIN_SYSTEM/src/Components/TrainDetails.jsx
@@ -6,6 +6,7 @@ function TrainDetails() {
   const { id } = useParams();
   const [train, setTrain] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchTrainData = async () => {
@@ -16,6 +17,7 @@ function TrainDetails() {
         setLoading(false);
       } catch (error) {
         console.error("Error fetching train details:", error);
+        setError("Failed to fetch train details. Please try again later.");
         setLoading(false);
       }
     };
@@ -25,32 +27,40 @@ function TrainDetails() {
 
   return (
     <div className="container mx-auto p-4">
-      {loading ? (
-        <div>Loading...</div>
-      ) : train ? (
-        <div className="max-w-md mx-auto bg-white shadow-md rounded-md p-6">
-          <div className="mb-4">
-            <h2 className="text-2xl font-bold">{train.trainName}</h2>
-            <p className="text-gray-600">Train Number: {train.trainNumber}</p>
-          </div>
-          <div className="border-t border-gray-300 pt-4">
-            <p className="text-lg">Delayed By: {train.delayedBy} minutes</p>
-            <p className="mt-2">
-              Departure Time:{" "}
-              {`${train.departureTime.Hours}:${train.departureTime.Minutes}:${train.departureTime.Seconds}`}
-            </p>
-            <p className="mt-2">
-              Seats Available: Sleeper = {train.seatsAvailable.sleeper}, AC ={" "}
-              {train.seatsAvailable.AC}
-            </p>
-            <p className="mt-2">
-              Price: AC = ${train.price.AC}, Sleeper = ${train.price.sleeper}
-            </p>
-          </div>
-        </div>
-      ) : (
-        <div>Train not found</div>
-      )}
+      <div className="max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
+        {loading && <div className="text-center">Loading...</div>}
+        {error && <div className="text-center text-red-500">{error}</div>}
+        {train && (
+          <>
+            <h2 className="text-2xl font-bold mb-4">{train.trainName}</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-gray-600">Train Number:</p>
+                <p>{train.trainNumber}</p>
+                <p className="text-gray-600 mt-2">Departure Time:</p>
+                <p>
+                  {`${train.departureTime.Hours}:${train.departureTime.Minutes}:${train.departureTime.Seconds}`}
+                </p>
+              </div>
+              <div>
+                <p className="text-gray-600">Delayed By:</p>
+                <p>{train.delayedBy} minutes</p>
+                <p className="text-gray-600 mt-2">Seats Available:</p>
+                <p>
+                  Sleeper: {train.seatsAvailable.sleeper}, AC:{" "}
+                  {train.seatsAvailable.AC}
+                </p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <p className="text-gray-600">Price:</p>
+              <p>
+                AC: ${train.price.AC}, Sleeper: ${train.price.sleeper}
+              </p>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- TRAIN_SYSTEM/src/Components/TrainListItem.jsx
+++ TRAIN_SYSTEM/src/Components/TrainListItem.jsx
@@ -2,27 +2,36 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function TrainListItem({ train }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+
   function handleTrainClick(trainNumber) {
     navigate(`/trains/${trainNumber}`);
   }
+
   return (
-    <li onClick={() => handleTrainClick(train.trainNumber)}>
-      <div>Train Number: {train.trainNumber}</div>
-      <div>Delayed By: {train.delayedBy}</div>
-      <div>Train Name: {train.trainName}</div>
-      <div>
+    <div
+      className="bg-white rounded-lg p-4 shadow-md hover:shadow-lg cursor-pointer transition duration-300"
+      onClick={() => handleTrainClick(train.trainNumber)}
+    >
+      <h2 className="text-xl font-semibold mb-2">{train.trainName}</h2>
+      <p className="text-gray-500 mb-1">Train Number: {train.trainNumber}</p>
+      <p className="text-gray-500 mb-1">
         Departure Time:{" "}
         {`${train.departureTime.Hours}:${train.departureTime.Minutes}:${train.departureTime.Seconds}`}
+      </p>
+      <p className="text-gray-500 mb-2">
+        Delayed By: {train.delayedBy} minutes
+      </p>
+      <div className="flex justify-between">
+        <p className="text-gray-700">
+          Seats Available: Sleeper = {train.seatsAvailable.sleeper}, AC ={" "}
+          {train.seatsAvailable.AC}
+        </p>
+        <p className="text-gray-700">
+          Price: AC = ${train.price.AC}, Sleeper = ${train.price.sleeper}
+        </p>
       </div>
-      <div>
-        Seats Available: Sleeper = {train.seatsAvailable.sleeper}, AC ={" "}
-        {train.seatsAvailable.AC}
-      </div>
-      <div>
-        Price: AC = ${train.price.AC}, Sleeper = ${train.price.sleeper}
-      </div>
-    </li>
+    </div>
   );
 }
 
